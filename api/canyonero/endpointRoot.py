4ad6c852d8bd9ee4ba7eb32b7f579aedb9500f5a
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-07-25 11:23:20 -0400

    Root Endpoint done

--- api/canyonero/endpointRoot.py
+++ api/canyonero/endpointRoot.py
@@ -1,15 +1,15 @@
+from flask import request
 from flask_restful import Resource
-import json
 
 class Root(Resource):
     """ Return the list of available top-level endpoint
     """
     def get(self):
         links = [
-            {'Links': 
-             {'href': '/nameset/',
+            {'link': 
+             {'href': request.base_url + 'nameset/',
               'rel': 'collection',
               'title': 'Name Sets',
-              'type': 'text/json'}
+              'type': 'application/json'}
              }]
-        return json.dumps(links)
+        return links
--- api/tests/test_root.py
+++ api/tests/test_root.py
@@ -1,5 +1,6 @@
 import unittest
 import canyonero
+import json
 
 class Test_RootEndpoint(unittest.TestCase):
     def setUp(self):
@@ -8,8 +9,13 @@ class Test_RootEndpoint(unittest.TestCase):
 
     def test_get(self):
         rv = self.target.get('/')
-        print(rv)
-        assert b'Link' in rv.data
+        self.assertEqual(200, rv.status_code)
+
+        result = json.loads(rv.data.decode('utf-8'))
+        self.assertEqual(1, len(result))
+        self.assertIn('link', result[0])
+        self.assertIn('href', result[0]['link'])
+        self.assertIn('nameset', result[0]['link']['href'])
 
 if __name__ == '__main__':
     unittest.main()
